21acb6564a9b230962a2a7ab621a88068389462e
aberkanenazi <aberkane12@hotmail.fr>
2024-01-28 01:06:26 +0100

    End of the sign up view

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  const isAuth = false;
+  return (
+    <>
+      {isAuth ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+          <img
+            src="/assets/images/viking.jpg"
+            alt="auth_bg"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
 };
 
 export default AuthLayout;
