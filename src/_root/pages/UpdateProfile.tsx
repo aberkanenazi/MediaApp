aad7134833ea868985f2ad040f50ea0c19ded376
aberkanenazi <aberkane12@hotmail.fr>
2024-12-27 02:08:53 +0100

    Update title

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -7,7 +7,7 @@ const UpdateProfile = () => {
         <div className=" max-w-5xl flex-start gap-3 justify-start w-full">
           <img src="/assets/icons/edit.svg" width={36} height={36} alt="edit" />
           <h2 className="h3-bold md:h2-bold text-left w-full">
-            Update Profile (TO / DO )
+            Update Profile
           </h2>
         </div>
         <UserForm />
